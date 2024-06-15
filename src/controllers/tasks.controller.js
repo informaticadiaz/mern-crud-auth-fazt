2605124edf34b063adc301bef315c75987537405
Ignacio Diaz <informatica.diaz@gmail.com>
2024-06-15 17:26:28 -0300

    task crud

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,42 @@
+import Task from "../models/tasks.models.js";
+
+export const getTasks = async (req, res) => {
+    const tasks = await Task.find({
+        user: req.user.id
+    }).populate('user')
+    res.json(tasks)
+}
+
+export const createTask = async (req, res) => {
+    const { title, description, date } = req.body
+
+    const newTask = new Task({
+        title,
+        description,
+        date,
+        user: req.user.id
+    })
+    const savedTask =await newTask.save()
+    res.json(savedTask)
+}
+
+export const getTask = async (req, res) => {
+    const task = await Task.findById(req.params.id)
+    if(!task) return res.status(404).json({ message: 'Task not found' })
+    res.json(task)
+}
+
+
+export const updateTask = async (req, res) => {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    if(!task) return res.status(404).json({ message: 'Task not found' })
+    res.json(task)
+
+}
+
+export const deleteTask = async (req, res) => {
+    const task = await Task.findByIdAndDelete(req.params.id)
+    if(!task) return res.status(404).json({ message: 'Task not found' })
+    res.json(task)
+
+}
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
 import { authRequired } from "../middlewares/validateToken.js";
+import { 
+    getTasks,
+    getTask,
+    createTask,
+    deleteTask,
+    updateTask
+} from "../controllers/tasks.controller.js";
 
 const router = Router();
 
-router.get('/tasks', authRequired, (req, res) => {
-    res.send('Tasks')
-})
+router.get('/tasks', authRequired, getTasks )
+router.get('/tasks/:id', authRequired, getTask )
+router.post('/tasks', authRequired, createTask)
+router.delete('/tasks/:id', authRequired, deleteTask)
+router.put('/tasks/:id', authRequired, updateTask)
 
 export default router
